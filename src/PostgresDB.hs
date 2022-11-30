131d59c639f291ea920644c23d6fb3366bb64bd9
Antonius Anggito Arissaputro <antonius.anggito@ui.ac.id>
2022-11-30 21:39:29 +0700

    Delete UniqueTitle field db

--- src/PostgresDB.hs
+++ src/PostgresDB.hs
@@ -41,8 +41,6 @@ share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
         status Int
         server_id String
         created_by String
-        UniqueTitle title
-        
         deriving Show
 
     UserD
