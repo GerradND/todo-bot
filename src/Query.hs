d5df6057ec55ece53704b0156bfbd75f67c5b672
gerrad.natanael <gerrad.natanael@ui.ac.id>
2022-12-03 09:26:07 +0700

    Add function type

--- src/Query.hs
+++ src/Query.hs
@@ -2,30 +2,40 @@
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE DataKinds #-}
 
 module Query (checkUncheckQuery) where
 
-import Calamity (tell)
-import Calamity.Commands
-import Calamity.Metrics.Noop
-import Control.Concurrent
-import Optics
-import Control.Monad
-import qualified Data.Text as T
+import           Calamity (tell, BotC)
+import           Calamity.Commands
+import           Calamity.Commands.Context (FullContext)
+import           Calamity.Metrics.Noop
+import           Control.Concurrent
+import           Control.Monad
+import           Data.Char (isDigit)
+import qualified Data.Text                   as T
+import           Database
+import           Database.Persist
+import           Database.Persist.Postgresql as Psql
+import           Database.Persist.TH
+import           Database.Persist.Types
 import qualified Di
-import qualified DiPolysemy as DiP
-import Database.Persist
-import Database.Persist.Postgresql as Psql
-import Database.Persist.TH
-import Database.Persist.Types
-import PostgresDB
-import Database
-import Data.Char (isDigit) 
+import           Optics
+import qualified Polysemy                    as P
+import           PostgresDB
 
+checkUncheckQuery :: 
+    ( BotC r
+    , P.Members
+    '[ Persistable
+    ] r
+    ) => FullContext -> T.Text -> Int -> P.Sem r ()
 checkUncheckQuery ctx txt status = do 
     let message = T.unpack $ ctx ^. #message % #content
     let lengthMessage = length . words $ message
-    if lengthMessage == 2 && (all isDigit $ T.unpack txt)
+    if lengthMessage == 2 && (T.all isDigit txt)
         then do
             let todoId = toSqlKey . read . T.unpack $ txt
             todo <- db $ get todoId
@@ -41,4 +51,10 @@ checkUncheckQuery ctx txt status = do
                         printResponse ctx "Todo unchecked!"
     else printResponse ctx "Wrong format!"
 
+printResponse ::   
+    ( BotC r
+    , P.Members
+    '[ Persistable
+    ] r
+    ) => FullContext -> T.Text -> P.Sem r ()
 printResponse ctx txt = void $ tell @T.Text ctx $ txt
